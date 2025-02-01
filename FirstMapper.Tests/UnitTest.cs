4c64b3cdb691cd81d66c472a6719d61303ab6d26
Pen-y-Fan <40126936+Pen-y-Fan@users.noreply.github.com>
2025-02-01 12:23:55 +0000

    Refactor and clean up tests, dependencies, and configs
    Removed unused files and redundant code, improving readability and organization. Updated `.gitignore` to include JetBrains-specific files. Refactored and restructured test cases to ensure proper naming conventions and framework alignment.

---
+++ FirstMapper.Tests/UnitTest.cs
@@ -0,0 +1,27 @@
+﻿using Shouldly;
+
+namespace FirstMapper.Tests;
+
+public class UnitTest
+{
+    [Fact]
+    public void TestXUnitIsWorking()
+    {
+        // Arrange
+        const bool truthy = true;
+
+        // Assert
+        Assert.True(truthy);
+    }
+
+    [Fact]
+    public void TestShudlyIsWorking()
+    {
+        // Arrange
+        const bool truthy = true;
+
+        // ReSharper disable once ConditionIsAlwaysTrueOrFalse
+        // Assert
+        truthy.ShouldBe(true, "There is a problem with Shouldly.");
+    }
+}
